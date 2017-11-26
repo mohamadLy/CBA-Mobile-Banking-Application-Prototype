e167b300e176bb0423eca92c7cb21b608c656506
Mohamadou Ly <mly083@uottawa.ca>
2017-11-25 23:16:44 -0500

    added bank logo

--- app/src/main/java/seg_capstone/cbamobilebankingapplicationprototype/Activities/MainActivity.java
+++ app/src/main/java/seg_capstone/cbamobilebankingapplicationprototype/Activities/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        toolbar.setLogo(R.drawable.cbc_logo);
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
