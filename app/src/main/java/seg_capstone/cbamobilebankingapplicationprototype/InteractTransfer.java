4451d1d5ce67e166abbf95d270d7c058fa37143e
Mohamadou Ly <mly083@uottawa.ca>
2017-11-27 21:09:51 -0500

    interac transfer files added

---
+++ app/src/main/java/seg_capstone/cbamobilebankingapplicationprototype/InteractTransfer.java
@@ -0,0 +1,13 @@
+package seg_capstone.cbamobilebankingapplicationprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InteractTransfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interact_transfer);
+    }
+}
